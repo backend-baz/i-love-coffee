542d0007938a055a801efb4e79db7a8b6a5da9a7
Amirhossein Emadi <emadi.henker404@gmail.com>
2025-09-12 22:04:27 -0700

    Refactor CoffeesController to include dynamic route for coffee by ID

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,10 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
     // GET: /coffees
-    @Get('/flavors')
+    @Get()
     findAll(): string {
         return 'This action returns all coffees.';
+    }
+
+    // GET: /coffees/:id
+    @Get(':id')
+    findOne(@Param('id') id: string): string {
+        return `This action returns #${id} coffee.`;
     }
 }
