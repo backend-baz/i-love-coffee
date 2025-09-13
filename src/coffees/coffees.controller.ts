bc672887cf81ad246a06927385cd4e166ea99ef8
Amirhossein Emadi <emadi.henker404@gmail.com>
2025-09-13 06:13:06 -0700

    add 2 endpoints (DELETE and PATCH) in the CoffeesController
    refactor CoffeesController for improved readability and consistency

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,22 +1,42 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
-    // GET: /coffees
-    @Get()
-    findAll(): string {
-        return 'This action returns all coffees.';
-    }
+  // GET -> /coffees
+  @Get()
+  findAll(): string {
+    return 'This action returns all coffees.';
+  }
 
-    // GET: /coffees/:id
-    @Get(':id')
-    findOne(@Param('id') id: string): string {
-        return `This action returns #${id} coffee.`;
-    }
+  // GET -> /coffees/:id
+  @Get(':id')
+  findOne(@Param('id') id: string): string {
+    return `This action returns #${id} coffee.`;
+  }
 
-    // POST: /coffees
-    @Post()
-    create(@Body('name') body): string {
-        return 'This action creats a coffee.';
-    }
+  // POST -> /coffees
+  @Post()
+  create(@Body('name') body): string {
+    return 'This action creates a coffee.';
+  }
+
+  // PATCH -> /coffees/:id
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body): string {
+    return `This action updates #${id} coffee.`;
+  }
+
+  // DELETE -> /coffees/:id
+  @Delete(':id')
+  remove(@Param('id') id: string): string {
+    return `This action removes #${id} coffee.`;
+  }
 }
