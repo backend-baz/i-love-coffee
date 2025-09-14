7978465f78bce3f44c9b4f864697ac4497edfac7
Amirhossein Emadi <emadi.henker404@gmail.com>
2025-09-14 08:13:57 -0700

    add validation to CreateCoffeeDto
    implement UpdateCoffeeDto using PartialType

--- src/coffees/dto/create-coffee.dto.ts
+++ src/coffees/dto/create-coffee.dto.ts
@@ -1,5 +1,31 @@
+import {
+  IsString,
+  MinLength,
+  MaxLength,
+  IsNotEmpty,
+  IsAlpha,
+  IsArray,
+} from 'class-validator';
+
 export class CreateCoffeeDto {
+  @IsString()
+  @IsNotEmpty()
+  @IsAlpha('en-US')
+  @MinLength(2)
+  @MaxLength(25)
   readonly name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsAlpha('en-US')
+  @MinLength(2)
+  @MaxLength(50)
   readonly brand: string;
+
+  @IsArray()
+  @IsString({ each: true })
+  @IsNotEmpty({ each: true })
+  @IsAlpha('en-US', { each: true })
+  @MinLength(3, { each: true })
   readonly flavors: string[];
 }
